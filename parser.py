81275d86e3e8d97c8b481b744dfd6cb325e15e75
James Bradbury <jamesbradbury93@gmail.com>
2021-07-12 17:23:29 +0100

    change to mpm from pico

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-from pico import copy_to_output, copy_main_patcher
+from mpm import copy_to_output, copy_main_patcher
 
 patch = Path("/Users/james/Cloud/Projects/impulse/training.maxpat")
 output_folder = Path('/Users/james/Cloud/Projects/impulse/training_example')
